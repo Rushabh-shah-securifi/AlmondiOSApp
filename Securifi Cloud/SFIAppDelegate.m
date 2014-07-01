46f90aabc565a82d557025fce5d32aa706177d52
sinclairday <msd@msdservices.com>
2014-07-01 14:25:01 +0800

    Added a formal "shutdown" method. On termination, the app now shutsdown the SDK.

--- Securifi Cloud/SFIAppDelegate.m
+++ Securifi Cloud/SFIAppDelegate.m
@@ -57,6 +57,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [[SecurifiToolkit sharedInstance] shutdown];
 }
 
 #pragma mark - Set up
