0a960a577606c9060163604ec10749dfcde522b8
sinclairday <msd@msdservices.com>
2014-07-21 17:14:48 +0800

    Clean up

--- Securifi Cloud/UI/InitialSlidingViewController.m
+++ Securifi Cloud/UI/InitialSlidingViewController.m
@@ -7,21 +7,11 @@
 //
 
 #import "InitialSlidingViewController.h"
-#import "SNLog.h"
-#import "MBProgressHUD.h"
-
-@interface InitialSlidingViewController ()
-@property(nonatomic, readonly) MBProgressHUD *HUD;
-@end
 
 @implementation InitialSlidingViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    _HUD = [[MBProgressHUD alloc] initWithView:self.view];
-    _HUD.dimBackground = YES;
-    [self.view addSubview:_HUD];
 
     UIStoryboard *storyboard;
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
@@ -32,43 +22,6 @@
     }
 
     self.topViewController = [storyboard instantiateViewControllerWithIdentifier:@"TabTop"];
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-
-//    UITabBarController *ctrl = (UITabBarController *) self.topViewController;
-//    ctrl.tabBar.tintColor = [UIColor blackColor];
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(reachabilityDidChange:)
-                                                 name:kSFIReachabilityChangedNotification
-                                               object:nil];
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
-
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:kSFIReachabilityChangedNotification object:nil];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
-    return YES;
-}
-
-#pragma mark - Event handlers
-
-- (void)reachabilityDidChange:(NSNotification *)notification {
-    if ([[SFIReachabilityManager sharedManager] isReachable]) {
-        DLog(@"Reachable - SLIDE");
-        self.HUD.labelText = @"Reconnecting...";
-        [self.HUD hide:YES afterDelay:1];
-    }
-    else {
-        DLog(@"Unreachable");
-    }
 }
 
 @end
