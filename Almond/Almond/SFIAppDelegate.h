71bd0a080748769a5a93d6c499c0bee1371e01d8
sinclairday <msd@msdservices.com>
2014-08-27 16:30:50 +0800

    reorganizing the tree; intorducing cocoapods for dependency management; new project file

---
+++ Almond/Almond/SFIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFIAppDelegate.h
+//  Almond
+//
+//  Created by Matthew Sinclair-Day on 8/26/14.
+//  Copyright (c) 2014 Securifi Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Almond/Almond/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Almond
+//
+//  Created by Matthew Sinclair-Day on 8/26/14.
+//  Copyright (c) 2014 Securifi Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SFIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SFIAppDelegate class]));
+    }
+}
