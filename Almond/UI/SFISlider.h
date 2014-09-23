f75d8a39e1de5ff16cdc11cd2c3198543af589e2
sinclairday <msd@msdservices.com>
2014-09-23 21:42:03 +0800

    Make the slider a subclass of ASValueTrackingSlider

--- Almond/UI/SFISlider.h
+++ Almond/UI/SFISlider.h
@@ -4,10 +4,11 @@
 //  Created by sinclair on 8/5/14.
 //
 #import <Foundation/Foundation.h>
+#import "ASValueTrackingSlider.h"
 
 
 // A slider that has a larger thumb area for tracking touch events
-@interface SFISlider : UISlider
+@interface SFISlider : ASValueTrackingSlider
 
 @property(nonatomic) SFIDevicePropertyType propertyType;
 
