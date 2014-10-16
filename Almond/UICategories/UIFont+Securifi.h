314847fafac0714a0b41cefb0d65b3141a43afcf
PriyaYerunkar <priya.yerunkar@securifi.com>
2014-10-16 13:09:11 +0530

    UIFont+Securifi category added

---
+++ Almond/UICategories/UIFont+Securifi.h
@@ -0,0 +1,13 @@
+//
+//  UIFont+Securifi.h
+//  Almond
+//
+//  Created by Securifi-Mac2 on 16/10/14.
+//  Copyright (c) 2014 Securifi Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIFont_Securifi : NSObject
+
+@end
---
+++ Almond/UICategories/UIFont+Securifi.m
@@ -0,0 +1,13 @@
+//
+//  UIFont+Securifi.m
+//  Almond
+//
+//  Created by Securifi-Mac2 on 16/10/14.
+//  Copyright (c) 2014 Securifi Ltd. All rights reserved.
+//
+
+#import "UIFont+Securifi.h"
+
+@implementation UIFont_Securifi
+
+@end
