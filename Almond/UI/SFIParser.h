ea1af4fd1bef320984a9297226512afc42a6dbe6
sinclairday <msd@msdservices.com>
2014-11-11 10:32:53 +0800

    Clean up: introduced enum to represent command and response types. Reformatted code to conform with codebase.

--- Almond/UI/SFIParser.h
+++ Almond/UI/SFIParser.h
@@ -19,37 +19,38 @@
 #import "SFIRouterSummary.h"
 
 @interface SFIParser : NSObject <NSXMLParserDelegate>
-    @property (nonatomic, retain) NSMutableString               *currentNodeContent;
-    @property (nonatomic, retain) NSMutableArray                *sensors;
-    @property (nonatomic, retain) NSMutableArray                *knownValues;
-    @property (nonatomic, retain) NSXMLParser                   *parser;
-    @property (nonatomic, retain) SFISensor                     *currentSensor;
-    @property (nonatomic, retain) SFIDeviceKnownValues          *currentKnownValue;
-    @property (nonatomic, retain) SFIRouterReboot               *routerReboot;
-    @property (nonatomic, retain) SFIDevicesList                *connectedDevices;
-    @property (nonatomic, retain) NSMutableArray                *connectedDevicesArray;
-    @property (nonatomic, retain) SFIConnectedDevice            *currentConnectedDevice;
-    @property (nonatomic, retain) SFIGenericRouterCommand       *genericCommandResponse;
+@property(nonatomic, retain) NSMutableString *currentNodeContent;
+@property(nonatomic, retain) NSMutableArray *sensors;
+@property(nonatomic, retain) NSMutableArray *knownValues;
+@property(nonatomic, retain) NSXMLParser *parser;
+@property(nonatomic, retain) SFISensor *currentSensor;
+@property(nonatomic, retain) SFIDeviceKnownValues *currentKnownValue;
+@property(nonatomic, retain) SFIRouterReboot *routerReboot;
+@property(nonatomic, retain) SFIDevicesList *connectedDevices;
+@property(nonatomic, retain) NSMutableArray *connectedDevicesArray;
+@property(nonatomic, retain) SFIConnectedDevice *currentConnectedDevice;
+@property(nonatomic, retain) SFIGenericRouterCommand *genericCommandResponse;
 
-    //PY 121113
-    @property (nonatomic, retain) SFIDevicesList                *blockedDevices;
-    @property (nonatomic, retain) NSMutableArray                *blockedDevicesArray;
-    @property (nonatomic, retain) SFIBlockedDevice              *currentBlockedDevice;
+//PY 121113
+@property(nonatomic, retain) SFIDevicesList *blockedDevices;
+@property(nonatomic, retain) NSMutableArray *blockedDevicesArray;
+@property(nonatomic, retain) SFIBlockedDevice *currentBlockedDevice;
 
-    //PY 131113
-    @property (nonatomic, retain) SFIDevicesList                *blockedContent;
-    @property (nonatomic, retain) NSMutableArray                *blockedContentArray;
-    @property (nonatomic, retain) SFIBlockedContent              *currentBlockedContent;
+//PY 131113
+@property(nonatomic, retain) SFIDevicesList *blockedContent;
+@property(nonatomic, retain) NSMutableArray *blockedContentArray;
+@property(nonatomic, retain) SFIBlockedContent *currentBlockedContent;
 
-    @property (nonatomic, retain) SFIDevicesList                *wirelessSettings;
-    @property (nonatomic, retain) NSMutableArray                *wirelessSettingsArray;
-    @property (nonatomic, retain) SFIWirelessSetting             *currentWirelessSetting;
+@property(nonatomic, retain) SFIDevicesList *wirelessSettings;
+@property(nonatomic, retain) NSMutableArray *wirelessSettingsArray;
+@property(nonatomic, retain) SFIWirelessSetting *currentWirelessSetting;
 
-    //PY 27113 - Router summary
-    @property (nonatomic, retain) SFIRouterSummary              *routerSummary;
-    @property (nonatomic, retain) NSMutableArray                *wirelessSummaryArray;
-    @property (nonatomic, retain) SFIWirelessSummary            *currentWirelessSummary;
+//PY 27113 - Router summary
+@property(nonatomic, retain) SFIRouterSummary *routerSummary;
+@property(nonatomic, retain) NSMutableArray *wirelessSummaryArray;
+@property(nonatomic, retain) SFIWirelessSummary *currentWirelessSummary;
 
--(NSMutableArray *)loadDataFromXML:(NSString*)xmlFileName;
--(SFIGenericRouterCommand *)loadDataFromString:(NSString*)xmlString;
+- (NSMutableArray *)loadDataFromXML:(NSString *)xmlFileName;
+
+- (SFIGenericRouterCommand *)loadDataFromString:(NSString *)xmlString;
 @end
