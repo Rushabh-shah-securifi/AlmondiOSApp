5d56b184da539dd40bc121e26fd2861d8e52800a
sinclairday <msd@msdservices.com>
2014-11-19 17:02:59 +0800

    starting build out of notifications UI

---
+++ Almond/Notifications/SFINotificationTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SFINotificationTableViewCell.m
+//  Almond
+//
+//  Created by Matthew Sinclair-Day on 11/19/14.
+//  Copyright (c) 2014 Securifi Ltd. All rights reserved.
+//
+
+#import "SFINotificationTableViewCell.h"
+
+@implementation SFINotificationTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
