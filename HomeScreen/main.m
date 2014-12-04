3f4f2949429d0736da2d19a93f6705fe623563fa
sinclairday <msd@msdservices.com>
2014-12-04 15:56:35 +0800

    Reorganzing build process to make room for branded apps. Converted icons to image assets and reorgnized/renamed them, esp. sensor icons. More work to be done.

---
+++ HomeScreen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HomeScreen
+//
+//  Created by Matthew Sinclair-Day on 12/3/14.
+//  Copyright (c) 2014 Securifi Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SFIAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SFIAppDelegate class]));
+    }
+}
