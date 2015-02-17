531dc5c21912ffa014c20a3079680165bfc654d3
sinclairday <msd@msdservices.com>
2015-02-17 12:39:36 +0800

    Clean up and doc

--- SecurifiApp/UI/SFITableViewController.h
+++ SecurifiApp/UI/SFITableViewController.h
@@ -12,10 +12,12 @@
 
 // Provides a base class for controllers attached to the main tab view.
 // This controller places a Cloud Status bar button in the right corner and manages its state.
+// It also places a Notification status button and manages its state.
 // A 'reveal' button is attached to the left corner for toggling the Reveal View controller.
 // A HUD is attached to the navigation controller, and subclasses should use it.
 // The controller is configured with the standard tint color and attribute style, and subclasses may find them by
 // interrogating the navigation bar's button items and title attributes.
+// Subclasses must call markAlmondMac any time the almond being managed is changed.
 @interface SFITableViewController : UITableViewController
 
 @property(nonatomic, readonly) MBProgressHUD *HUD;
@@ -37,10 +39,11 @@
 // convenience method for showing a standard "updating settings..." message
 - (void)showUpdatingSettingsHUD;
 
-// Disabled by default
+// Disabled by default; when YES, a status button indicating the number of open Notifications is shown, allowing
+// the user to tap to present the Notification Viewer.
 @property(nonatomic) BOOL enableNotificationsView;
 
-// Enabled by default
+// Enabled by default; when NO, the Reveal button is disabled. Useful when the UI needs to be locked during updates.
 @property(nonatomic) BOOL enableDrawer;
 
 @end
