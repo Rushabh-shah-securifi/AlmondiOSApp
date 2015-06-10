fff73fdc3e4828c54d213624dd01d6deff0fc8da
Matthew Sinclair-Day <msd@mingfu.tw>
2015-06-10 15:10:29 +0800

    Added ability to register a standard UITextFieldDelegate even when the field is configured for "only numbers" mode

--- SecurifiApp/UI/SFITextField.m
+++ SecurifiApp/UI/SFITextField.m
@@ -7,6 +7,7 @@
 
 
 @interface SFITextField () <UITextFieldDelegate>
+@property(nonatomic, weak) id <UITextFieldDelegate> registeredDelegate;
 @end
 
 @implementation SFITextField
@@ -20,25 +21,95 @@
     return self;
 }
 
+#pragma mark - Special delegate handling
+
+- (void)setDelegate:(id <UITextFieldDelegate>)delegate {
+    if (delegate == self) {
+        return;
+    }
+    self.registeredDelegate = delegate;
+    super.delegate = delegate;
+}
+
+#pragma mark - Mode
+
 - (void)setMode:(SFITextFieldMode)mode {
     _mode = mode;
 
     if (mode == SFITextFieldMode_numbersOnly || mode == SFITextFieldMode_numbersInRange) {
         self.keyboardType = UIKeyboardTypeNumberPad;
-        self.delegate = self;
+        super.delegate = self;
     }
+    else if (self.registeredDelegate) {
+        super.delegate = self.registeredDelegate;
+    }
+    else {
+        super.delegate = nil;
+    }
+}
+
+#pragma mark - UITextFieldDelegate methods
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    id <UITextFieldDelegate> de = self.registeredDelegate;
+    if (de && [de respondsToSelector:@selector(textFieldShouldBeginEditing:)]) {
+        [de textFieldShouldBeginEditing:textField];
+    }
+    return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    id <UITextFieldDelegate> de = self.registeredDelegate;
+    if (de && [de respondsToSelector:@selector(textFieldDidBeginEditing:)]) {
+        [de textFieldDidBeginEditing:textField];
+    }
+}
+
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+    id <UITextFieldDelegate> de = self.registeredDelegate;
+    if (de && [de respondsToSelector:@selector(textFieldShouldEndEditing:)]) {
+        [de textFieldShouldEndEditing:textField];
+    }
+    return YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    id <UITextFieldDelegate> de = self.registeredDelegate;
+    if (de && [de respondsToSelector:@selector(textFieldDidEndEditing:)]) {
+        [de textFieldDidEndEditing:textField];
+    }
+}
+
+- (BOOL)textFieldShouldClear:(UITextField *)textField {
+    id <UITextFieldDelegate> de = self.registeredDelegate;
+    if (de && [de respondsToSelector:@selector(textFieldShouldClear:)]) {
+        [de textFieldShouldClear:textField];
+    }
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    id <UITextFieldDelegate> de = self.registeredDelegate;
+    if (de && [de respondsToSelector:@selector(textFieldShouldReturn:)]) {
+        [de textFieldShouldReturn:textField];
+    }
+    return YES;
 }
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
     // validate numbers and number range only
     //
     if (self.mode != SFITextFieldMode_numbersOnly && self.mode != SFITextFieldMode_numbersInRange) {
+        id <UITextFieldDelegate> de = self.registeredDelegate;
+        if (de && [de respondsToSelector:@selector(textField:shouldChangeCharactersInRange:replacementString:)]) {
+            return [de textField:textField shouldChangeCharactersInRange:range replacementString:string];
+        }
         return YES;
     }
 
     NSCharacterSet *nonNumberSet = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
     BOOL onlyNumbers = [string stringByTrimmingCharactersInSet:nonNumberSet].length == string.length;
-    
+
     if (!onlyNumbers) {
         return NO;
     }
