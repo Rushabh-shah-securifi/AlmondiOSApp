8ec8afda8f3541ed2fc851dec0c0d67f3ea74727
Matthew Sinclair-Day <msd@mingfu.tw>
2015-06-11 11:53:57 +0800

    Updated for support for Update Firmware Response

--- SecurifiApp/Legacy/SFIGenericRouterCommand.h
+++ SecurifiApp/Legacy/SFIGenericRouterCommand.h
@@ -16,6 +16,7 @@ typedef NS_ENUM(unsigned int, SFIGenericRouterCommandType) {
     SFIGenericRouterCommandType_WIRELESS_SETTINGS       = 7,
     SFIGenericRouterCommandType_WIRELESS_SUMMARY        = 9,
     SFIGenericRouterCommandType_SEND_LOGS_RESPONSE      = 10,
+    SFIGenericRouterCommandType_UPDATE_FIRMWARE_RESPONSE = 11,
 };
 
 @interface SFIGenericRouterCommand : NSObject
@@ -24,5 +25,6 @@ typedef NS_ENUM(unsigned int, SFIGenericRouterCommandType) {
 
 @property(nonatomic) BOOL commandSuccess;
 @property(nonatomic) NSString *responseMessage;
+@property(nonatomic) unsigned int completionPercentage;
 
 @end
