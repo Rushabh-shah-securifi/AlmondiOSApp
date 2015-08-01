40591dcf96ca97368ea807ebaebaf9234c601798
Matthew Sinclair-Day <msd@mingfu.tw>
2015-08-01 11:59:55 +0800

    Made inclusion of Scenes a configurable option

--- SecurifiApp/SFIAppDelegate.m
+++ SecurifiApp/SFIAppDelegate.m
@@ -30,6 +30,7 @@
     config.enableNotificationsHomeAwayMode = YES;
 //    config.enableRouterWirelessControl = YES;       // YES by default
     config.enableLocalNetworking = YES;
+    config.enableScenes = YES;
     return config;
 }
 
