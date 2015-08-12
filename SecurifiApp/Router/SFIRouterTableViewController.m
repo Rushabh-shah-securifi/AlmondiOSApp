74a36c1358d7d2749538258348dcd2ced16d2b4b
Matthew Sinclair-Day <msd@mingfu.tw>
2015-08-12 10:54:48 +0800

    Merging back in legacy code that should remain in the UI until after next major release.

--- SecurifiApp/Router/SFIRouterTableViewController.m
+++ SecurifiApp/Router/SFIRouterTableViewController.m
@@ -66,6 +66,8 @@ typedef NS_ENUM(unsigned int, AlmondSupportsSendLogs) {
 @property enum RouterViewState routerViewState;
 
 @property(nonatomic, strong) SFIRouterSummary *routerSummary;
+@property(nonatomic, strong) NSArray *connectedDevices;     // SFIConnectedDevice
+@property(nonatomic, strong) NSArray *blockedDevices;       // SFIBlockedDevice
 
 @property NSNumber *currentExpandedSection; // nil == none expanded
 @property NSUInteger currentExpandedCount; // number of rows in expanded section
@@ -156,6 +158,9 @@ typedef NS_ENUM(unsigned int, AlmondSupportsSendLogs) {
 
     // init state
     self.routerSummary = nil;
+    self.connectedDevices = nil;
+    self.blockedDevices = nil;
+
     self.currentExpandedSection = nil;
     self.currentExpandedCount = 0;
     self.allowCellExpandControl = YES;
@@ -443,7 +448,7 @@ typedef NS_ENUM(unsigned int, AlmondSupportsSendLogs) {
         case DEF_WIRELESS_SETTINGS_SECTION:
             return 1;
         case DEF_DEVICES_AND_USERS_SECTION:
-            return 1 + self.currentExpandedCount;
+            return 1;
         case DEF_ROUTER_VERSION_SECTION:
             return 1 + self.currentExpandedCount;
         case DEF_ROUTER_REBOOT_SECTION:
@@ -1218,6 +1223,20 @@ typedef NS_ENUM(unsigned int, AlmondSupportsSendLogs) {
         }
 
         switch (genericRouterCommand.commandType) {
+            case SFIGenericRouterCommandType_CONNECTED_DEVICES: {
+                SFIDevicesList *ls = genericRouterCommand.command;
+                self.connectedDevices = ls.deviceList;
+                [self syncCheckRouterViewState:RouterViewReloadPolicy_always];
+                break;
+            }
+
+            case SFIGenericRouterCommandType_BLOCKED_MACS: {
+                SFIDevicesList *ls = genericRouterCommand.command;
+                self.blockedDevices = ls.deviceList;
+                [self syncCheckRouterViewState:RouterViewReloadPolicy_always];
+                break;
+            }
+
             case SFIGenericRouterCommandType_WIRELESS_SETTINGS: {
                 SFIDevicesList *ls = genericRouterCommand.command;
                 NSArray *settings = ls.deviceList;
