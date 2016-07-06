fb06257554838064825781c2ff167140504a65d5
Masood <masood.pathan@securifi.com>
2016-07-06 16:52:25 +0530

    client properties nav bar color change

--- SecurifiApp/Sensor/sensor table/ClientPropertiesViewController.m
+++ SecurifiApp/Sensor/sensor table/ClientPropertiesViewController.m
@@ -47,6 +47,9 @@
 int randomMobileInternalIndex;
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationController.navigationBar.tintColor = [SFIColors ruleBlueColor];
+    self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+    
     self.clientPropertiesTable.backgroundColor = self.genericParams.color;
     self.resetView.backgroundColor = self.genericParams.color;
     [self.resetButton setTitleColor: self.genericParams.color forState:UIControlStateNormal];
@@ -150,6 +153,9 @@ int randomMobileInternalIndex;
 -(void)viewWillAppear:(BOOL)animated{
     NSLog(@"client properties - view will appear");
     [super viewWillAppear:YES];
+    self.navigationController.navigationBar.tintColor = [SFIColors ruleBlueColor];
+    self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+    
     randomMobileInternalIndex = arc4random() % 10000;
     [self initializeNotifications];
     _toolkit=[SecurifiToolkit sharedInstance];
