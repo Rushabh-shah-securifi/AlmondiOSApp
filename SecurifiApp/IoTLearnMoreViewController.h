6aa1716d2ca7517f2558fc39343bff8e50f1571a
Masood <masood.pathan@securifi.com>
2016-12-28 11:59:39 +0530

    added iot learn more files

---
+++ SecurifiApp/IoTLearnMoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  IoTLearnMoreViewController.h
+//  SecurifiApp
+//
+//  Created by Masood on 12/28/16.
+//  Copyright © 2016 Securifi Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IoTLearnMoreViewController : UIViewController
+
+@end
