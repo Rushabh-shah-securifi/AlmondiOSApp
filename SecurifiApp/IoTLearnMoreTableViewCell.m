246f257daed1fff56cad98f472aca788e9d514c4
Masood <masood.pathan@securifi.com>
2017-01-18 13:08:05 +0530

    hyperlink selection fix

--- SecurifiApp/IoTLearnMoreTableViewCell.m
+++ SecurifiApp/IoTLearnMoreTableViewCell.m
@@ -33,7 +33,6 @@
     NSLog(@"setIssueCell: type %@",type);
      if([type isEqualToString:@"2"] || [type isEqualToString:@"6"]){
          self.issueTitle.text = [CommonMethods type:type];
-        NSLog(@"text == %@",[CommonMethods getExplanationText:type]);
         self.issueDesc.attributedText = [self setLink:[CommonMethods getExplanationText:type] link:@"https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers" label:self.issueDesc];
         NSLog(@"text == %@",self.issueDesc.attributedText);
     }else{
@@ -60,12 +59,8 @@
     
     [attrStr addAttribute:NSLinkAttributeName value:link range:NSMakeRange(attrStr.length-5, 4)];
     [attrStr addAttribute:NSUnderlineStyleAttributeName value:@(NSUnderlineStyleSingle) range:NSMakeRange(attrStr.length-5, 4)];
-    
     [attrStr addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:NSMakeRange(attrStr.length-5, 4)];
-    
-    NSLog(@"attrString is %@",attrStr);
     return attrStr;
-    
 }
 
 @end
